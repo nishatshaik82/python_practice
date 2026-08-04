5df95a81a15c514613f04df05063f83b0b18f80b
nishatshaik82 <nishatshaik82@gmail.com>
2026-08-04 12:13:58 +0530

    update files

---
+++ oops concept/abstract.py
@@ -0,0 +1,24 @@
+class Shape:
+    
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,r):
+       self.r = r
+
+    def area(self):
+       return 3.14* self.r * self.r
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+       self.l = l
+       self.w = w           
+    def area(self):
+       return self.l * self.w
+shapes = [
+    Circle(5),
+    Rectangle(4,6)    
+]
+for shape in shapes:
+    print(shape.area())    
